e46bdf569a3c901a01b3ba8a08de8794e62874ba
Max Sun <max.sun@yale.edu>
2019-05-15 22:44:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |number|
-    new_array << array[number - 1] ** 1# ** 2#(array[number - 1])
+    new_array << array[number - 1] ** 3# ** 2#(array[number - 1])
   end
   return new_array
 end
